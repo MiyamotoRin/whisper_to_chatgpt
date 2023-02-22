c48066bb6f53331f15b350abec926c88e7181089
MiyamotoRin <67382813+MiyamotoRin@users.noreply.github.com>
2023-02-22 16:16:40 +0900

    test: 仮想環境のtorchがCUDAに対応する

--- mov_summerize.py
+++ mov_summerize.py
@@ -6,7 +6,7 @@ import youtube_dl
 
 input_model_size = 'small' #whisperのモデルのサイズ
 
-input_url = 'https://www.youtube.com/watch?v=DerrX7XoLDc'
+input_url = 'https://www.youtube.com/watch?v=5Xxtq6dkG4g'
 output_file = 'result_' + input_model_size + '.txt'
 source_language = 'Japanese'
 
@@ -22,17 +22,17 @@ ydl_opts = {
     ],
 }
 
-tmp_audio = 'yt_tmp.mp3'
-if(os.path.exists(tmp_audio)):
-    os.remove(tmp_audio)
+tmp_audio = "yt_tmp.mp3"
+# if(os.path.exists(tmp_audio)):
+#     os.remove(tmp_audio)
 
 # この行を追加してPyTorchでCPUの利用を強制する
 # torch.cuda.is_available = lambda: False
 
 try:
     # YouTubeから動画をダウンロード
-    ydl = youtube_dl.YoutubeDL(ydl_opts)
-    info_dict = ydl.extract_info(input_url, download=True)
+    # ydl = youtube_dl.YoutubeDL(ydl_opts)
+    # info_dict = ydl.extract_info(input_url, download=True)
     
     # 文字起こしのモデルを読み込む
     transcription_model = whisper.load_model(input_model_size)
@@ -42,7 +42,7 @@ try:
     
     # make log-Mel spectrogram and move to the same device as the model
     # わからなかったのでコピペ、多分CPUかGPUかの選択
-    mel = whisper.log_mel_spectrogram(transcription_audio).to(transcription_model.device)
+    mel = whisper.log_mel_spectrogram(transcription_audio).to("cuda")
     
     options = whisper.DecodingOptions(fp16=False)
     result = whisper.decode(transcription_model, mel, options)
