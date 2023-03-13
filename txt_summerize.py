59009f4dccf3ebfa69bf458f12be21b76ea99b63
MiyamotoRin <67382813+MiyamotoRin@users.noreply.github.com>
2023-03-13 15:43:20 +0900

    test: 文字起こしする言語を入れたら、要約してくれるインターフェースの完成

--- txt_summerize.py
+++ txt_summerize.py
@@ -5,16 +5,20 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# UTF8エンコードのテキストファイルの読み込み
-with open('./tmp.txt', encoding='utf-8') as f:
-    text = f.read()
+def summerize():
+  # UTF8エンコードのテキストファイルの読み込み
+  with open('./tmp.txt', encoding='utf-8') as f:
+      text = f.read()
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Please summerize this text."},
-    {"role": "user", "content": text}
-  ]
-)
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "user", "content": "Please summerize this text."},
+      {"role": "user", "content": text}
+    ]
+  )
 
-print(completion.choices[0].message.content)
+  print(completion.choices[0].message.content)
+
+  with open("./sum.txt", mode='w', encoding='utf-8') as f:
+      f.write(completion.choices[0].message.content)
